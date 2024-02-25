06d5df04ce298fef4863fb218ce491d901b27a74
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-26 00:14:26 +0300

    Feat:Added table creation
    I just used simple SQL

--- migrations/2024-02-25-210503_users/up.sql
+++ migrations/2024-02-25-210503_users/up.sql
@@ -1,1 +1,9 @@
 -- Your SQL goes here
+-- Create a database for users called watu
+CREATE TABLE watu (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    funguo TEXT NOT NULL,
+    vault TEXT NOT NULL,
+    UNIQUE(email)
+);
