3b55e1337883bbbc361edb45f6e1dbe174eed984
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-26 00:19:01 +0300

    Feat:MAde sure DB connection works
    I will now update the README.md

--- migrations/2024-02-25-210503_users/up.sql
+++ migrations/2024-02-25-210503_users/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 -- Create a database for users called watu
 CREATE TABLE watu (
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL PRIMARY KEY,
     email TEXT NOT NULL,
     funguo TEXT NOT NULL,
     vault TEXT NOT NULL,
---
+++ src/schema.rs
@@ -0,0 +1,10 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    watu (id) {
+        id -> Int4,
+        email -> Text,
+        funguo -> Text,
+        vault -> Text,
+    }
+}
