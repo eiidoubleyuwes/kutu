b7e0f12e2858a80a8382d5dd6ac4f273bd9fcb49
eiidoubleyuwes <i.am.mbuguaa34@gmail.com>
2024-02-26 12:22:55 +0300

    Feat:Added necessary imports

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-#[macro_use] extern crate rocket;
-
+#[macro_use] 
+extern crate rocket;
+use diesel::prelude::*;
+mod schema;
+mod models;
+#[macro_use] 
+extern crate diesel;
 #[get("/")] // Default endpoint
 fn index() -> &'static str {
     "More testing"
